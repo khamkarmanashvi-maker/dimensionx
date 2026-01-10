965f37b951a83002467e838dde07ad46b69a42ca
khamkarmanashvi-maker <khamkarmanashvi@gmail.com>
2026-01-10 20:50:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@
     "https://vanshk17.app.n8n.cloud/form/ded330e2-d8c7-4f5a-8781-776aa3fdef1e",
     "_blank"
   );
+
+}
+function startMockInterview() {
+  window.location.href = "https://abhichaurasiya91.github.io/AI-INTERVIEWER/";
 }
